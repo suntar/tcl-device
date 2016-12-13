efb61343276b78d8bc9b17c9f741d2cf7aae5433
Vladislav Zavjalov <slazav@altlinux.org>
2016-12-14 01:16:01 +0200

    Device: rewrite Device package (has to be tested!)

--- Device/device.tcl
+++ Device/device.tcl
@@ -1,10 +1,10 @@
 package provide Device 1.1
 package require Itcl
 package require Locking
-package require GpibLib
-package require Graphene
 
-# Open any device, send scpi command and read response, close device.
+source drivers.tcl
+
+# Open any device, send command, read response, close device.
 #
 # Usage:
 #  Device lockin0
@@ -13,13 +13,9 @@ package require Graphene
 # Information about devices read from devices.txt file:
 # name model driver parameters
 #
-# Supported drivers and parameters:
-#  gpib_prologix <hostname>:<gpib_address> -- gpib device connected
-#                         to prologix gpib to ethernet converter
-#  lxi_scpi_raw <hostname> -- scpi_raw interface to lxi device
-#  tenma_ps <char device>  -- tenma power supply
-#
-# Locking is done on every single input/output operation using Rota's Locking library
+# Locking is done on every single input/output operation
+# using Rota's Locking library.
+# Higher-level locking may be needed on high-level operations
 #
 itcl::class Device {
   variable dev;    # device handle
@@ -57,191 +53,53 @@ itcl::class Device {
       }
     }
     if { $drv eq "" } { error "Can't find device $name in /etc/devices.txt"}
-
-    # open device
-    switch $drv {
-     gpib_prologix {
-       set pp [split $pars ":"]
-       set host      [lindex $pp 0]
-       set gpib_addr [lindex $pp 1]
-       set dev [::socket $host 1234]
-       fconfigure $dev -blocking false -buffering line
-     }
-     lxi_scpi_raw {
-       set host $pars
-       set dev [::socket $host 5025]
-       fconfigure $dev -blocking false -buffering line
-     }
-     tenma_ps {
-       set dev [::open $pars RDWR]
-       fconfigure $dev -blocking false  -buffering line
-     }
-     gpib {
-       set dev [gpib_device gpib::$name {*}$pars]
-     }
-     graphene {
-       set dev [graphene::open {*}$pars]
-     }
-     pico_rec {
-       set dev [::open "| pico_rec -d $pars" RDWR]
-       read_line_nb $dev 3000
-     }
-     default {puts "Unknown driver name in devices.txt"}
-    }
-
-    # initialize lock
-    set lock [lock_init #auto $name]
-  }
-
-  destructor {
-    switch $drv {
-      gpib_prologix { ::close $dev}
-      lxi_scpi_raw  { ::close $dev}
-      tenma_ps      { ::close $dev}
-      gpib          { gpib_device delete $dev}
-      graphene      { graphene::close $dev}
-      pico_rec      { ::close $dev}
-    }
+    set dev [conn_drivers::$drv #auto $pars]
+    set lock [lock_init #auto device_lib_lock_for_$name]; # initialize lock
   }
 
 
   ####################################################################
   # run command, read response if needed
   method write {c} {
-
     after 1000 {
       puts "Device locking timeout"
       return
     }
     $lock wait
     $lock get
-    set ret {}
-
-    switch $drv {
-     gpib_prologix {
-       puts $dev $c
-       flush $dev
-     }
-     lxi_scpi_raw {
-       puts $dev $c
-       flush $dev
-     }
-     tenma_ps {
-       # no newline characters, timeouts are important!
-       puts -nonewline $dev $c
-       flush $dev
-     }
-     gpib { $dev write $c }
-     pico_rec {
-       puts $dev $c
-       flush $dev
-     }
-     default {set ret "write is not supported by driver $drv" }
-    }
+    $dev write $msg
     $lock release
     return $ret
   }
 
   ####################################################################
   # run command, read response if needed
-  method cmd_read {c} {
-
+  method cmd {msg} {
     after 1000 {
       puts "Device locking timeout"
       return
     }
     $lock wait
     $lock get
-    set ret {}
-
-    switch $drv {
-     gpib_prologix {
-       puts $dev $c
-       flush $dev
-       set ret [read_line_nb $dev 1000]
-     }
-     lxi_scpi_raw {
-       puts $dev $c
-       flush $dev
-       set ret [read_line_nb $dev 1000]
-     }
-     pico_rec {
-       puts $dev $c
-       flush $dev
-       set ret [read_line_nb $dev 1000]
-     }
-     tenma_ps {
-       # no newline characters, timeouts are important!
-       puts -nonewline $dev $c
-       flush $dev
-       after 50
-       set ret [read $dev 1024]
-     }
-     gpib {
-       set ret [$dev cmd_read $c]
-     }
-     graphene {
-       set ret [graphene::cmd $dev $c]
-     }
-     default {set ret "cmd_read is not supported by driver $drv" }
-    }
+    set ret [ $dev cmd $msg ]
     $lock release
     return $ret
   }
+  # alias
+  method cmd_read {msg} { cmd $msg }
 
   ####################################################################
   # run command, read response if needed
   method read {c} {
-
     after 1000 {
       puts "Device locking timeout"
       return
     }
     $lock wait
     $lock get
-    set ret {}
-
-    switch $drv {
-     pico_rec {
-       set ret [read_line_nb $dev 1000]
-     }
-     default {set ret "read is not supported by driver $drv" }
-    }
+    set ret [$dev read ]
     $lock release
     return $ret
-  }
-
-
-
-  ####################################################################
-  ## read a line until \n character or timeout
-  ## dev should be configured with -blocking 0
-  private method read_line_nb {dev timeout} {
-    while {$timeout>0} {
-      gets $dev res
-      if { [string length $res] } {
-        return $res
-      }
-      after 10
-      set timeout [expr {$timeout-10}]
-    }
-  }
-
-  # send command, read response if needed
-  private method send_cmd_auto {dev cmd} {
-    puts $dev $cmd
-    flush $dev
-    if {[string index $cmd end] == "?"} {
-      return [read_line_nb $dev 1000]
-    }
-    return {}
-  }
-
-  # send command and read response 
-  private method send_cmd_read {dev cmd} {
-    puts $dev $cmd
-    flush $dev
-    return [read_line_nb $dev 1000]
   }
 
 }
---
+++ Device/drivers.tcl
@@ -0,0 +1,205 @@
+## Connection drivers for the Device library.
+## Here we do not want to know device commands,
+## but only know how to open/close connection and read/write commands
+
+package require Itcl
+package require GpibLib
+package require Graphene
+
+
+namespace eval conn_drivers {
+
+####################################################################
+## read a line until \n character or timeout
+## dev should be configured with -blocking 0
+proc read_line_nb {dev timeout} {
+  while {$timeout>0} {
+    gets $dev res
+    if { [string length $res] } {
+      return $res
+    }
+    after 10
+    set timeout [expr {$timeout-10}]
+  }
+}
+
+###########################################################
+# GPIB device connected through Prologix gpib2eth converter
+# parameters:
+#  -hostname -- converter hostname or ip-address
+#  -addr     -- device GPIB address
+#  -read_timeout -- read timeout, ms
+itcl::class gpib_prologix {
+  variable dev
+  variable addr
+  variable read_timeout
+
+  # open device
+  constructor {pars} {
+    set pp [split $pars ":"]
+    set host      [lindex $pp 0]
+    set gpib_addr [lindex $pp 1]
+    set dev [::socket $host 1234]
+    set read_timeout 1000
+    fconfigure $dev -blocking false -buffering line
+  }
+  # close device
+  destructor {
+    ::close $dev
+  }
+  # set address before any operation
+  method set_addr {} {
+    puts $dev "++addr"
+    flush $dev
+    set a [read_line_nb $dev $read_timeout]
+    if { $a != $addr } {
+      puts $dev "++addr $addr"
+      flush $dev
+    }
+  }
+  # write to device without reading answer
+  method write {msg} {
+    set_addr()
+    puts $dev $msg
+    flush $dev
+  }
+  # read from device
+  method read {} {
+    set_addr
+    return [read_line_nb $dev $read_timeout]
+  }
+  # write and then read
+  method cmd {msg} {
+    set_addr
+    write $msg
+    return [read_line_nb $dev $read_timeout]
+  }
+}
+
+###########################################################
+# LXI device connected via ethernet. SCPI raw connection via port 5025
+# parameters:
+#  -hostname -- device hostname or ip-address
+#  -read_timeout -- read timeout, ms
+itcl::class lxi_scpi_raw {
+  variable dev
+  variable read_timeout
+
+  # open device
+  constructor {pars} {
+    set host $pars
+    set dev [::socket $host 5025]
+    fconfigure $dev -blocking false -buffering line
+  }
+  # close device
+  destructor {
+    ::close $dev
+  }
+  # write to device without reading answer
+  method write {msg} {
+    puts $dev $msg
+    flush $dev
+  }
+  # read from device
+  method read {} {
+    return [read_line_nb $dev $read_timeout]
+  }
+  # write and then read
+  method cmd {msg} {
+    write $msg
+    return [read_line_nb $dev $read_timeout]
+  }
+}
+
+###########################################################
+# Connection with GpibLib
+# parameters same is in the GpibLib
+itcl::class gpib {
+  variable dev
+  constructor {pars} { set dev [gpib_device gpib::$name {*}$pars] }
+  destructor { gpib_device delete $dev }
+  method write {msg} { $dev write $msg }
+  method read {} { return [$dev read ] }
+  method cmd {msg} { return [$dev cmd_read $msg ] }
+}
+
+###########################################################
+# Graphene database. Parameter string is a command name for
+# running graphene interface:
+#   graphene -d . interactive
+#   ssh somehost graphene -d . interactive
+# TODO! - do not use graphene package
+itcl::class graphene {
+  variable dev
+  constructor {pars} { set dev [::graphene::open {*}$pars] }
+  destructor { ::graphene::close $dev }
+  method cmd {msg} { return [::graphene::cmd $dev $msg] }
+}
+
+###########################################################
+# pico_rec program. Parameter ptring is a command name
+itcl::class pico_rec {
+  variable dev
+  variable open_timeout 3000
+  variable read_timeout 1000
+
+  constructor {pars} {
+    set dev [::open "| pico_rec -d $pars" RDWR]
+    read_line_nb $dev $open_timeout
+  }
+  destructor {::close $dev}
+  method write {msg} {
+    puts $dev $msg
+    flush $dev
+  }
+  method read {} {
+    return [read_line_nb $dev $read_timeout]
+  }
+  method cmd {msg} {
+    puts $dev $c
+    flush $dev
+    return [read_line_nb $dev $read_timeout]
+  }
+}
+
+###########################################################
+# Tenma power supply. It is a serial port connection,
+# but with specific delays and without newline characters.
+itcl::class tenma_ps {
+  variable dev
+  variable del;     # read/write delay
+  variable bufsize; # read buffer size
+
+  # open device
+  constructor {pars} {
+    set dev [::open $pars RDWR]
+    set del 50
+    set bufsize 1024
+    fconfigure $dev -blocking false -buffering line
+  }
+  # close device
+  destructor {
+    ::close $dev
+  }
+  # write to device without reading answer
+  method write {msg} {
+    puts -nonewline $dev $msg; # no newline!
+    after $del
+    flush $dev
+  }
+  # read from device
+  method read {} {
+    after $del
+    return [read $dev $bufsize]
+  }
+  # write and then read
+  method cmd {msg} {
+    puts -nonewline $dev $msg
+    flush $dev
+    after $del
+    return [read $dev $bufsize]
+  }
+}
+###########################################################
+
+}; #namespace
