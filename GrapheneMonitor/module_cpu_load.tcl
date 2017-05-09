c58e746004bac4feb360983c3f40c74c81f99430
Vladislav Zavjalov <slazav@altlinux.org>
2017-05-09 13:58:37 +0300

    GrapheneMonitor/module_cpu_load.tcl: change defaults

--- GrapheneMonitor/module_cpu_load.tcl
+++ GrapheneMonitor/module_cpu_load.tcl
@@ -7,8 +7,8 @@ itcl::class cpu_load {
   constructor {} {
     set dbname cpu_load
     set tmin   1
-    set tmax   10
-    set atol   {0.01 0.01 0.01}
+    set tmax   100
+    set atol   {0.005 0.005 0.005}
     set name   "CPU load (/proc/loadavg)"
     set cnames {"1 min CPU load " "5 min CPU load" "10 min CPU load"}
   }
