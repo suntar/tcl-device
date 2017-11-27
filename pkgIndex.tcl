11d3f359336391769e076f376d311fee939bb5a9
Vladislav Zavjalov <slazav@altlinux.org>
2017-11-27 15:44:01 +0200

    add spp_server_async.tcl to pkgIndex.tcl

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -3,7 +3,7 @@
 
 set _name    Device
 set _version 1.6
-set _files   {spp_client spp_server device drivers chan locking}
+set _files   {spp_client spp_server spp_server_async device drivers chan locking}
 
 set _pcmd {}
 foreach _f $_files { lappend _pcmd "source [file join $dir $_f.tcl]" }
